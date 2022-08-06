1cf2b438f1c48f2ee7c6dbc96bf70c9e8c355cd4
Kuat Abdrau <kabdrau@MacBook-Pro.local>
2022-08-06 14:13:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+//Object Destructuring
+//What does the following code return/print?
+
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+let planetFacts = {
+	numPlanets: 8,
+	yearNeptuneDiscovered: 1846,
+	yearMarsDiscovered: 1659
+};
+  
+let {numPlanets: numPlanets2, ...discoveryYears} = planetFacts;
+  
+console.log(discoveryYears); // {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+function getUserData({firstName, favoriteColor="green"}){
+	return `Your name is ${firstName} and you like ${favoriteColor}`;
+}
+  
+getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // 'Your name is Alejandro and you like purple'
+getUserData({firstName: "Melissa"}) // 'Your name is Melissa and you like green'
+getUserData({}) // 'Your name is undefined and you like green'
+
+//Array Destructuring
+//What does the following code return/print?
+
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // "Maya"
+console.log(second); // "Marisa"
+console.log(third); // "Chi"
+
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+	"Raindrops on roses",
+	"whiskers on kittens",
+	"Bright copper kettles",
+	"warm woolen mittens",
+	"Brown paper packages tied up with strings"
+]
+  
+console.log(raindrops); // "Raindrops on roses"
+console.log(whiskers); // "whiskers on kittens"
+console.log(aFewOfMyFavoriteThings); // ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // [10, 30, 20]
+
+//ES5 Assigning Variables to Object Properties
+var obj = {
+	numbers: {
+		a: 1,
+		b: 2
+	}
+};
+  
+var a = obj.numbers.a;
+var b = obj.numbers.b;
+
+//ES2015 Object Destructuring
+const {a:a1,b:b1} = obj.numbers;
+
+//ES5 Array Swap
+var arr = [1, 2];
+// var temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
+
+//ES2015 One-Line Array Swap with Destructuring
+[arr[0], arr[1]] = [arr[1], arr[0]]
+console.log(arr)
+
+//raceResults()
+const raceResults = ([first, second, third, ...rest]) => {
+	return{first, second, third, rest}
+}
